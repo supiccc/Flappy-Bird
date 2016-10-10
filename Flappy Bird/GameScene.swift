4b3f1ba713cd5a7ea7632f306df2e1eff3fd9686
supiccc <supiccc@gmail.com>
2016-10-10 22:55:29 +0800

    变量名替换为英语

--- Flappy Bird/GameScene.swift
+++ Flappy Bird/GameScene.swift
@@ -8,46 +8,46 @@
 
 import SpriteKit
 
-enum 图层: CGFloat {
-    case 背景
-    case 前景
-    case 游戏角色
+enum photos: CGFloat {
+    case background
+    case frontground
+    case roleOfGame
 }
 
 
 class GameScene: SKScene {
     
-    let 世界单位 = SKNode()
-    var 游戏区域起始点: CGFloat = 0
-    var 游戏区域的高度: CGFloat = 0
+    let nodeOfWorld = SKNode()
+    var starOfGame: CGFloat = 0
+    var heightOfGame: CGFloat = 0
     
     
     override func didMoveToView(view: SKView) {
-        addChild(世界单位)
-        设置背景()
-        设置前景()
+        addChild(nodeOfWorld)
+        setBackground()
+        setFrontground()
     }
     
     // MARK: 设置的相关方法
     
-    func 设置背景() {
-        let 背景 = SKSpriteNode(imageNamed: "Background")
-        背景.anchorPoint = CGPoint(x: 0.5, y: 1.0)
-        背景.position = CGPoint(x: size.width/2, y: size.height)
-        背景.zPosition = 图层.背景.rawValue
-        世界单位.addChild(背景)
+    func setBackground() {
+        let background = SKSpriteNode(imageNamed: "Background")
+        background.anchorPoint = CGPoint(x: 0.5, y: 1.0)
+        background.position = CGPoint(x: size.width/2, y: size.height)
+        background.zPosition = photos.background.rawValue
+        nodeOfWorld.addChild(background)
         
-        游戏区域起始点 = size.height - 背景.size.height
-        游戏区域的高度 = 背景.size.height
+        starOfGame = size.height - background.size.height
+        heightOfGame = background.size.height
         
     }
     
-    func 设置前景() {
-        let 前景 = SKSpriteNode(imageNamed: "Ground")
-        前景.anchorPoint = CGPoint(x: 0, y: 1.0)
-        前景.position = CGPoint(x: 0, y: 游戏区域起始点)
-        前景.zPosition = 图层.前景.rawValue
-        世界单位.addChild(前景)
+    func setFrontground() {
+        let frontground = SKSpriteNode(imageNamed: "Ground")
+        frontground.anchorPoint = CGPoint(x: 0, y: 1.0)
+        frontground.position = CGPoint(x: 0, y: starOfGame)
+        frontground.zPosition = photos.frontground.rawValue
+        nodeOfWorld.addChild(frontground)
     }
     
     
--- Flappy Bird/GameViewController.swift
+++ Flappy Bird/GameViewController.swift
@@ -13,19 +13,19 @@ class GameViewController: UIViewController {
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
         
-        if let sk视图 = self.view as? SKView {
-            if sk视图.scene == nil {
+        if let ViewOfGame = self.view as? SKView {
+            if ViewOfGame.scene == nil {
                 //  创建场景
-                let 长宽比 = sk视图.bounds.size.height / sk视图.bounds.size.width
-                let 场景 = GameScene(size:CGSize(width: 320, height: 320 * 长宽比))
-                sk视图.showsFPS = true
-                sk视图.showsNodeCount = true
-                sk视图.showsPhysics = true
-                sk视图.ignoresSiblingOrder = true
+                let boundsOfView = ViewOfGame.bounds.size.height / ViewOfGame.bounds.size.width
+                let SceneOfGame = GameScene(size:CGSize(width: 320, height: 320 * boundsOfView))
+                ViewOfGame.showsFPS = true
+                ViewOfGame.showsNodeCount = true
+                ViewOfGame.showsPhysics = true
+                ViewOfGame.ignoresSiblingOrder = true
                 
-                场景.scaleMode = .AspectFill
+                SceneOfGame.scaleMode = .AspectFill
                 
-                sk视图.presentScene(场景)
+                ViewOfGame.presentScene(SceneOfGame)
             }
         }
         
